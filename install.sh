713c10a23b7ee8708e40f0b0c18d6e623d2a5a96
Tim Clifford <tclifford@protonmail.com>
2020-11-10 17:22:06 +0000

    Actually commit what was meant to be in the last commit

--- install.sh
+++ install.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-git submodule init
-git submodule update --recursive
 if [ -f $HOME/.vimrc ]; then
 	mv $HOME/.vimrc $HOME/.vimrc.orig
 fi
@@ -8,20 +6,16 @@ if [ -L $HOME/.vimrc ]; then
 	rm $HOME/.vimrc
 fi
 ln -s "$(pwd)/.vimrc" $HOME
-if [ ! -d $HOME/.vim ]; then
-	mkdir $HOME/.vim
-fi
-if [ ! -d $HOME/.vim/bundle ]; then
-	mkdir $HOME/.vim/bundle
-fi
-if [ -L $HOME/.vim/bundle/Vundle.vim ]; then
-	rm $HOME/.vim/bundle/Vundle.vim
-fi
-if [ ! -d $HOME/.vim/bundle/Vundle.vim ]; then
-	ln -s Vundle.vim $HOME/.vim/bundle
-fi
+
+mkdir -p $HOME/.vim/pack/vendor/start
+git clone --depth 1 \
+	https://github.com/VundleVim/Vundle.vim.git \
+	$HOME/.vim/pack/vendor/start/vundle
+
 vim +PluginInstall +qall
-ln -s $HOME/.vim/bundle/jupyter-vim/ipython-magic/plot_to_pdf.py $HOME/.ipython/profile_default/startup
+if [ -d $HOME/.ipython ]; then
+	ln -s $HOME/.vim/bundle/jupyter-vim/ipython-magic/plot_to_pdf.py $HOME/.ipython/profile_default/startup
+fi
 cd $HOME/.vim/bundle/youcompleteme/
 python3 install.py --all
 cd -
